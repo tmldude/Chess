f4d5f72c88422dd959a9cece619e922dfbffc3ec
Tmldude <tl688@cornell.edu>
2022-07-04 11:03:02 -0400

    Added skeleton functions

---
+++ Chess/Main.py
@@ -0,0 +1,23 @@
+import pygame
+import time
+import sys
+
+import Piece
+import Board
+
+board = [['  ' for i in range(8)] for i in range(8)]
+
+
+'''adds all the pieces to the appropriate squares'''
+def board_setup():
+    raise NameError("Unimplemented")
+
+
+'''prints the board function'''
+def print_board():
+    raise NameError("Unimplemented")
+
+
+'''move(selected_index). Takes in selected index and runs the move algorithm in pieces for the piece'''
+def move(selected_index):
+    raise NameError("Unimplemented")
---
+++ Chess/Piece.py
@@ -0,0 +1,49 @@
+
+'''The piece class'''
+class Pieces:
+    def __int__(self, name, color, image, is_king = False):
+        self.name = name
+        self.color = color
+        self.image = image
+        self.is_king = is_king
+
+
+    '''The individual piece functions output ALL possible moves on a 8x8 chess board for the piece at the 
+    given index. This is a list:[(int,int)] or a list of move indexes. After, the are_moves_valid() function
+    should be ran to remove all illegal moves that may jump over pieces of go past them.'''
+
+
+    '''Pawn move function: takes in index of selected pawn outputs possible pawn moves'''
+    def pawn_move(self, index: (int, int)) -> list[(int, int)]:
+        raise NameError("Unimplemented")
+
+    '''Knight move function: takes in index of selected knight and outputs possible knight moves'''
+    def knight_move(self, index: (int, int)) -> list[(int, int)]:
+        raise NameError("Unimplemented")
+
+    '''Bishop move function: takes in index of selected bishop and outputs possible bishop moves'''
+    def bishop_move(self, index: (int, int)) -> list[(int, int)]:
+        raise NameError("Unimplemented")
+
+    '''Rook move function: takes in index of selected rook and outputs possible rook moves'''
+    def rook_move(self, index: (int, int)) -> list[(int, int)]:
+        raise NameError("Unimplemented")
+
+    '''Queen move function: takes in index of selected queen and outputs possible queen moves'''
+    def bishop_move(self, index: (int, int)) -> list[(int, int)]:
+        raise NameError("Unimplemented")
+
+    '''King move function: takes in index of selected king and outputs possible king moves'''
+    def king_move(self, index: (int, int)) -> list[(int, int)]:
+        raise NameError("Unimplemented")
+
+    '''is_valid(index,moves,board): Takes in the index of the piece to be moved, the possible moves
+     list[(int,int)] for a piece and tests the board to see if the possible_moves are valid. 
+     This function eliminates moves that intersect with other pieces of the same color and the 
+     squares after that intersection. Additionally, it stops at the first piece of the opposing color and
+     eliminates the moves beyond this point'''
+    def are_moves_valid(self, moves: list[(int, int)], board) -> list[(int,int)]:
+        raise NameError("Unimplemented")
+
+
+
